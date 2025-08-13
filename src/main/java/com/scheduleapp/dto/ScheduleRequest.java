c01a33b1e001323533fb71381107c9d97012fafc
jinhee kim <lsa68803@naver.com>
2025-08-13 11:15:19 +0900

    feat(dto) : [ScheduleRequest] 작성 완료

---
+++ src/main/java/com/scheduleapp/dto/ScheduleRequest.java
@@ -0,0 +1,11 @@
+package com.scheduleapp.dto;
+
+import lombok.Getter;
+
+@Getter
+public class ScheduleRequest {
+
+    private String name;
+    private String title;
+    private String content;
+}
