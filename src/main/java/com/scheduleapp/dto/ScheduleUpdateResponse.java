723bc75cd007854fffa71ef1911de094b9ff6ad4
jinhee kim <lsa68803@naver.com>
2025-08-13 12:55:58 +0900

    feat : 수정Response 작성 완료

--- src/main/java/com/scheduleapp/dto/ScheduleUpdateResponse.java
+++ src/main/java/com/scheduleapp/dto/ScheduleUpdateResponse.java
@@ -1,4 +1,18 @@
 package com.scheduleapp.dto;
 
+import com.scheduleapp.entity.Schedule;
+import lombok.Getter;
+
+@Getter
 public class ScheduleUpdateResponse {
+
+    private Long id;
+    private String title;
+    private String name;
+
+    public ScheduleUpdateResponse(Long id, String title, String name) {
+        this.id = id;
+        this.title = title;
+        this.name = name;
+    }
 }
