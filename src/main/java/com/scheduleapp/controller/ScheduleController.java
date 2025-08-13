d0da826875513d2a8ac0c5943b5eb5abe3c55887
jinhee kim <lsa68803@naver.com>
2025-08-13 11:24:29 +0900

    feat(controller) : [ScheduleController- 'C'] 일정 생성 작성 완료

---
+++ src/main/java/com/scheduleapp/controller/ScheduleController.java
@@ -0,0 +1,38 @@
+package com.scheduleapp.controller;
+
+import com.scheduleapp.dto.ScheduleRequest;
+import com.scheduleapp.dto.ScheduleResponse;
+import com.scheduleapp.entity.Schedule;
+import com.scheduleapp.service.ScheduleService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class ScheduleController {
+
+    private final ScheduleService scheduleService;
+
+    // CRUD의 [C] -> 일정 생성(저장)
+    @PostMapping("/schedules")
+    public ResponseEntity<ScheduleResponse> createSchedule(
+            @RequestBody ScheduleRequest scheduleRequest
+    ){
+        return ResponseEntity.ok(scheduleService.save(scheduleRequest));
+    }
+
+
+    // CRUD의 [R] -> 일정 전체 조회
+
+
+    // CRUD의 [R] -> 일정 단건 조회
+
+
+    // CRUD의 [U] -> 일정 수정
+
+
+    // CRUD의 [D] -> 일정 삭제
+}
