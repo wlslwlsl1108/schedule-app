86553230094c86a88a228d842cd61f33435af924
jinhee kim <lsa68803@naver.com>
2025-08-13 11:53:43 +0900

    feat : [ScheduleController- 'R'] 일정 단건 조회 완료

--- src/main/java/com/scheduleapp/controller/ScheduleController.java
+++ src/main/java/com/scheduleapp/controller/ScheduleController.java
@@ -5,10 +5,7 @@ import com.scheduleapp.dto.ScheduleResponse;
 import com.scheduleapp.service.ScheduleService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -26,6 +23,7 @@ public class ScheduleController {
         return ResponseEntity.ok(scheduleService.save(scheduleRequest));
     }
 
+
     // CRUD의 [R] -> 일정 전체 조회 및 작성자명 조회
     @GetMapping("/schedules")
     public ResponseEntity<List<ScheduleResponse>> getSchedules(
@@ -40,6 +38,12 @@ public class ScheduleController {
 
 
     // CRUD의 [R] -> 일정 단건 조회
+    @GetMapping("/schedules/{schedulId}")
+    public ResponseEntity<ScheduleResponse> getSchedule(
+            @PathVariable Long scheduleId
+    ){
+        return ResponseEntity.ok(scheduleService.findSchedule(scheduleId));
+    }
 
 
     // CRUD의 [U] -> 일정 수정
